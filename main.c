131639caff7b8011e9e299ee92ec0acf720bce8c
ELEliseev <43805466+ELEliseev@users.noreply.github.com>
2020-04-08 23:07:48 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#define RELEASE
+
+#ifdef RELEASE
+#warning "Protection is ON. Debug is OFF"
+	    FLASH_OBProgramInitTypeDef obConfig;
+
+	    HAL_FLASHEx_OBGetConfig(&obConfig);
+
+	    if (obConfig.RDPLevel == OB_RDP_LEVEL_0) {
+	        // this is first time we run mcu after flashing firmware
+	        obConfig.RDPLevel = OB_RDP_LEVEL_1;
+	        HAL_FLASH_Unlock();
+	        HAL_FLASH_OB_Unlock();
+	        HAL_FLASHEx_OBProgram(&obConfig);
+	        HAL_FLASH_OB_Launch();
+	    }
+#endif
